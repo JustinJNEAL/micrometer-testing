d3bd8113ccc2e179efdff6769d762b4bade05169
Justin Neal <Justin.Neal@fisglobal.com>
2023-07-19 09:35:08 -0400

    Update example

--- src/main/java/com/fis/entapi/services/micrometer/MicrometerTestingApplication.java
+++ src/main/java/com/fis/entapi/services/micrometer/MicrometerTestingApplication.java
@@ -1,22 +1,21 @@
 package com.fis.entapi.services.micrometer;
 
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
+import io.micrometer.common.KeyValue;
+import io.micrometer.common.KeyValues;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.observation.ClientRequestObservationContext;
+import org.springframework.http.client.observation.DefaultClientRequestObservationConvention;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.Optional;
+
+import static org.springframework.http.HttpMethod.GET;
 
 @SpringBootApplication
 public class MicrometerTestingApplication {
@@ -29,38 +28,57 @@ public class MicrometerTestingApplication {
     @Bean
     ApplicationRunner applicationRunner(RestTemplate restTemplate) {
 
-        return args -> restTemplate.getForObject("https://www.google.com", String.class);
+        return args -> {
+
+            var httpHeaders = new HttpHeaders();
+            httpHeaders.set("testHeader", "header");
+
+            var httpEntity = new HttpEntity<Void>(httpHeaders);
+
+            restTemplate.exchange("https://www.google.com", GET, httpEntity, String.class);
+        };
     }
 
     @Bean
-    RestTemplate restTemplate(RestTemplateBuilder builder, Tracer tracer, ObservationRegistry observationRegistry) {
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
 
         return builder
-                .interceptors(new MicrometerInterceptor(tracer, observationRegistry))
                 .build();
+    }
+
+    @Bean
+    DefaultClientRequestObservationConvention defaultClientRequestObservationConvention() {
+
+        return new ExtendedClientRequestObservationConvention();
     }
 
 }
 
-@RequiredArgsConstructor
-class MicrometerInterceptor implements ClientHttpRequestInterceptor {
-
-    private final Tracer tracer;
-    private final ObservationRegistry observationRegistry;
+class ExtendedClientRequestObservationConvention extends DefaultClientRequestObservationConvention {
 
     @Override
-    public ClientHttpResponse intercept(HttpRequest httpRequest, @NotNull byte[] bytes,
-                                        ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
+    public KeyValues getLowCardinalityKeyValues(ClientRequestObservationContext context) {
 
-        var header = "testHeader";
+        return KeyValues.of(clientName(context), exception(context), method(context), outcome(context), status(context),
+                subscriber(context), uri(context));
+    }
 
-        Optional.ofNullable(tracer.currentSpan())
-                .ifPresent(currentSpan -> currentSpan.name(header));
+    private KeyValue subscriber(ClientRequestObservationContext context) {
 
-        Optional.ofNullable(observationRegistry.getCurrentObservation())
-                .ifPresent(observation -> observation.contextualName(header));
+        if (context.getCarrier() != null) {
 
-        return clientHttpRequestExecution.execute(httpRequest, bytes);
+            var testHeader = Optional.ofNullable(context.getCarrier().getHeaders().get("testHeader"))
+                    .orElseThrow(() -> new RuntimeException("missing header"))
+                    .stream()
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("unable to get header"));
+
+            return KeyValue.of("testHeader", testHeader);
+
+        } else {
+
+            return KeyValue.of("testHeader", KeyValue.NONE_VALUE);
+        }
     }
 
 }
